1fc03f7ae9d1745eb7e9b3e653cf5c928170a849
Joey Scanga <scangjo@gmail.com>
2025-03-06 13:43:29 -0600

    updated mapping to remove ;

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -3,6 +3,7 @@ require "nvchad.mappings"
 -- add yours here
 
 local map = vim.keymap.set
+local del = vim.keymap.del
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("n", "<C-h>",  "<cmd> TmuxNavigateLeft<CR>")
@@ -10,5 +11,7 @@ map("n", "<C-l>", "<cmd> TmuxNavigateRight<CR>" )
 map("n", "<C-j>", "<cmd> TmuxNavigateDown<CR>" )
 map("n", "<C-k>", "<cmd> TmuxNavigateUp<CR>")
 map("i", "jk", "<ESC>")
+
+del("n", ";")
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
