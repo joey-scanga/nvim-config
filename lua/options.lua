f1993ca051786d6e7d5b34c8a2414131b472ba39
Joey Scanga <scangaj@chadlap3.local.dhcp.wustl.edu>
2025-03-06 13:05:02 -0600

    init

---
+++ lua/options.lua
@@ -0,0 +1,11 @@
+require "nvchad.options"
+
+-- add yours here!
+
+local o = vim.o
+local g = vim.g
+
+g.slime_target = "tmux"
+
+o.rnu = true
+-- o.cursorlineopt ='both' -- to enable cursorline!
---
+++ lua/plugins/init.lua
@@ -0,0 +1,46 @@
+return {
+  {
+    "stevearc/conform.nvim",
+    -- event = 'BufWritePre', -- uncomment for format on save
+    opts = require "configs.conform",
+  },
+
+  -- These are some examples, uncomment them if you want to see them work!
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require "configs.lspconfig"
+    end,
+  },
+
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "vim",
+        "lua",
+        "vimdoc",
+        "html",
+        "css",
+        "python",
+        "bash",
+      },
+    },
+  },
+  {
+    "stevearc/oil.nvim",
+    ---@module 'oil'
+    ---@type oil.SetupOpts
+    opts = {},
+    -- Optional dependencies
+    dependencies = { { "echasnovski/mini.icons", opts = {} } },
+    -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
+    -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
+    lazy = false,
+  },
+  { "jpalardy/vim-slime", lazy = false },
+  {
+    "christoomey/vim-tmux-navigator",
+    lazy = false
+  },
+}
