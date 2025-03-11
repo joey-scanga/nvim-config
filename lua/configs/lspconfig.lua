9bc2af196f854cbddb70bf257f9b372131c3ffa8
Joey Scanga <scangjo@gmail.com>
2025-03-11 18:06:59 -0500

    added ruff to lsps

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -4,7 +4,7 @@ require("nvchad.configs.lspconfig").defaults()
 local lspconfig = require "lspconfig"
 
 -- EXAMPLE
-local servers = { "html", "cssls", "pylsp", "bashls", "esbonio" }
+local servers = { "html", "cssls", "pylsp", "bashls", "esbonio", "ruff" }
 local nvlsp = require "nvchad.configs.lspconfig"
 
 -- lsps with default config
