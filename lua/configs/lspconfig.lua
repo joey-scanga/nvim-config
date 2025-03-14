9548688dfcedbe122592ef1e74afe3926cc72252
Joey Scanga <scangjo@gmail.com>
2025-03-14 00:04:34 -0500

    added swift lsp

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -4,7 +4,7 @@ require("nvchad.configs.lspconfig").defaults()
 local lspconfig = require "lspconfig"
 
 -- EXAMPLE
-local servers = { "html", "cssls", "pylsp", "bashls", "esbonio", "ruff" }
+local servers = { "html", "cssls", "pylsp", "bashls", "esbonio", "ruff", "sourcekit" }
 local nvlsp = require "nvchad.configs.lspconfig"
 
 -- lsps with default config
