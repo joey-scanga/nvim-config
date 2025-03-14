06b20122dfa40f7d8641ba97d86e482a4990fcb0
Joey Scanga <scangjo@gmail.com>
2025-03-14 13:57:41 -0500

    Changed lspconfig to ignore excessive warnings

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -4,7 +4,8 @@ require("nvchad.configs.lspconfig").defaults()
 local lspconfig = require "lspconfig"
 
 -- EXAMPLE
-local servers = { "html", "cssls", "pylsp", "bashls", "esbonio" }
+-- local servers = { "html", "cssls", "pylsp", "bashls", "esbonio" }
+local servers = { "html", "cssls", "bashls", "esbonio" }
 local nvlsp = require "nvchad.configs.lspconfig"
 
 -- lsps with default config
@@ -16,6 +17,36 @@ for _, lsp in ipairs(servers) do
   }
 end
 
+lspconfig["pylsp"].setup {
+  on_attach = nvlsp.on_attach,
+  filetypes = { "python" },
+  on_init = nvlsp.on_init,
+  capabilities = nvlsp.capabilities,
+  settings = {
+    configurationSources = { "flake8" },
+    formatCommand = { "black" },
+    pylsp = {
+      plugins = {
+        -- jedi_completion = {fuzzy = true},
+        -- jedi_completion = {eager=true},
+        jedi_completion = {
+          include_params = true,
+        },
+        jedi_signature_help = { enabled = true },
+        pyflakes = { enabled = true },
+        -- pylint = {args = {'--ignore=E501,E231', '-'}, enabled=true, debounce=200},
+        pylsp_mypy = { enabled = false },
+        pycodestyle = {
+          enabled = true,
+          ignore = { "E501", "E231" },
+          maxLineLength = 120,
+        },
+        yapf = { enabled = true },
+        mccabe = { enabled = false },
+      },
+    },
+  },
+}
 -- configuring single server, example: typescript
 -- lspconfig.ts_ls.setup {
 --   on_attach = nvlsp.on_attach,
